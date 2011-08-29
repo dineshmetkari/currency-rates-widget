b532ceda1f8e6e5a4216a306fe477942a56f4096
rpagyc <rpagyc@gmail.com>
2011-08-29 13:57:46 +0000

    add onDisable function to kill alarmManager when widget is disabled

--- CurrencyRatesWidget/src/com/app/currencywidget/CurrencyRatesWidget.java
+++ CurrencyRatesWidget/src/com/app/currencywidget/CurrencyRatesWidget.java
@@ -1,7 +1,5 @@
 package com.app.currencywidget;
 
-import java.util.ArrayList;
-
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
@@ -19,64 +17,59 @@ public class CurrencyRatesWidget extends AppWidgetProvider {
 	
 	private static final String ACTION_WIDGET_RECEIVER = "com.app.currencywidget.ACTION_WIDGET_RECEIVER";
 	private RemoteViews currencyRatesView = new RemoteViews("com.app", R.layout.widget_layout);
-	private ArrayList<RemoteViews> currencyRatesList = new ArrayList<RemoteViews>();
+	private CurrencyRates currencyRates;
 		
 	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){		
-		updateWidget(context);
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){	
+		Log.d("DEBUG","onUpdate");	
 		
+		updateWidget(context);	
 	}		
 	
 	@Override
 	public void onEnabled(Context context){
+		Log.d("DEBUG","onEnabled");	
 		super.onEnabled(context);
 		
-		Intent fireIntent = new Intent(context, CurrencyRatesWidget.class);
-		fireIntent.setAction(ACTION_WIDGET_RECEIVER);
-		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, fireIntent, 0);
-		currencyRatesView.setOnClickPendingIntent(R.id.CurrencyRatesWidget, pendingIntent);
-		
 		AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);		
-		alarmManager.setRepeating(AlarmManager.RTC, SystemClock.elapsedRealtime() + 360000, 360000, pendingIntent);
-		
-		updateWidget(context);		
+		alarmManager.setRepeating(AlarmManager.RTC, SystemClock.elapsedRealtime() + 360000, 360000, createPendingIntent(context));		
 	}
-
+	
+	@Override 
+	public void onDisabled(Context context){
+		Log.d("DEBUG","onDisabled");			
+		super.onDisabled(context);
+		AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+		alarmManager.cancel(createPendingIntent(context));
+	}
+	
 	@Override
 	public void onReceive(Context context, Intent intent){
+		Log.d("DEBUG","onReceive");			
 		super.onReceive(context, intent);
 		
 		if(intent.getAction().equals(ACTION_WIDGET_RECEIVER)){			
-			updateWidget(context);						
+			updateWidget(context);	
 		}
 	}
 	
 	public void updateWidget(Context context){
 		Log.d("DEBUG","updateWidget");		
+		currencyRates = new CurrencyRates(context);		
+		currencyRatesView = currencyRates.getCurrencyRates(context);
 		
-		//currencyRatesView  = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-		//ArrayList<RemoteViews> currencyRatesList = new ArrayList<RemoteViews>();		
-		CurrencyRates currencyRates = new CurrencyRates(context);
+		currencyRatesView.setOnClickPendingIntent(R.id.CurrencyRatesWidget, createPendingIntent(context));
 		
-		currencyRatesList = currencyRates.getCurrencyRates(context);
-		
-		currencyRatesView.removeAllViews(R.id.widget_layout);
-		
-		for (int i = 0; i < currencyRatesList.size(); i++){			
-			currencyRatesView.addView(R.id.widget_layout, currencyRatesList.get(i));
-		}
-		
-		/*
-		Intent fireIntent = new Intent(context, CurrencyRatesWidget.class);
-		fireIntent.setAction(ACTION_WIDGET_RECEIVER);
-		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, fireIntent, 0);
-		currencyRatesView.setOnClickPendingIntent(R.id.CurrencyRatesWidget, pendingIntent);
-		*/
-		//AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);		
-		//alarmManager.setRepeating(AlarmManager.RTC, SystemClock.elapsedRealtime() + 5000, 5000, pendingIntent);
 		// Push update for this widget to the home screen
 		ComponentName thisAppWidget = new ComponentName(context.getPackageName(),getClass().getName());	
 		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
 		appWidgetManager.updateAppWidget(thisAppWidget, currencyRatesView);		
+	}
+	
+	private PendingIntent createPendingIntent(Context context){
+		Intent intent = new Intent(context, CurrencyRatesWidget.class);
+		intent.setAction(ACTION_WIDGET_RECEIVER);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		return pendingIntent;
 	}
 }
