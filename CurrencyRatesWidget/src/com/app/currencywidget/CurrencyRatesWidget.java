d969e71a62ce407132b784ae505c38137cf3ed23
rpagyc <rpagyc@gmail.com>
2011-08-30 22:04:07 +0000

    up down stock arrows added

--- CurrencyRatesWidget/src/com/app/currencywidget/CurrencyRatesWidget.java
+++ CurrencyRatesWidget/src/com/app/currencywidget/CurrencyRatesWidget.java
@@ -1,5 +1,7 @@
 package com.app.currencywidget;
 
+import java.util.ArrayList;
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
@@ -7,7 +9,9 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.SystemClock;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.RemoteViews;
 
@@ -18,52 +22,89 @@ public class CurrencyRatesWidget extends AppWidgetProvider {
 	private static final String ACTION_WIDGET_RECEIVER = "com.app.currencywidget.ACTION_WIDGET_RECEIVER";
 	private RemoteViews currencyRatesView = new RemoteViews("com.app", R.layout.widget_layout);
 	private CurrencyRates currencyRates;
+	private ArrayList<CurrencyRates> currencyRatesList = new ArrayList<CurrencyRates>();
+	private SharedPreferences prefs;
 		
 	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){	
-		Log.d("DEBUG","onUpdate");	
-		
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
 		updateWidget(context);	
 	}		
 	
 	@Override
-	public void onEnabled(Context context){
-		Log.d("DEBUG","onEnabled");	
-		super.onEnabled(context);
-		
+	public void onEnabled(Context context){		
+		super.onEnabled(context);		
 		AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);		
 		alarmManager.setRepeating(AlarmManager.RTC, SystemClock.elapsedRealtime() + 360000, 360000, createPendingIntent(context));		
 	}
 	
 	@Override 
-	public void onDisabled(Context context){
-		Log.d("DEBUG","onDisabled");			
+	public void onDisabled(Context context){					
 		super.onDisabled(context);
 		AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
 		alarmManager.cancel(createPendingIntent(context));
 	}
 	
 	@Override
-	public void onReceive(Context context, Intent intent){
-		Log.d("DEBUG","onReceive");			
-		super.onReceive(context, intent);
-		
+	public void onReceive(Context context, Intent intent){			
+		super.onReceive(context, intent);		
 		if(intent.getAction().equals(ACTION_WIDGET_RECEIVER)){			
 			updateWidget(context);	
 		}
 	}
 	
 	public void updateWidget(Context context){
-		Log.d("DEBUG","updateWidget");		
+		
 		currencyRates = new CurrencyRates(context);		
-		currencyRatesView = currencyRates.getCurrencyRates(context);
+		currencyRatesList = currencyRates.getCurrencyRates(context);
+				
+		prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		SharedPreferences.Editor prefEditor = prefs.edit();
+		
+		currencyRatesView.removeAllViews(R.id.widget_layout);
+		
+		for (int i = 0; i < currencyRatesList.size(); i++){
+			
+			RemoteViews ratesView = new RemoteViews(context.getPackageName(),R.layout.rates_frame_layout);
+			
+			String currencyName = currencyRatesList.get(i).getCurrencyName();
+			String buyRate = currencyRatesList.get(i).getBuyRate();
+			String sellRate = currencyRatesList.get(i).getSellRate();
+			String ext = currencyRatesList.get(i).getExt();
+			String oldCurrencyRate = prefs.getString(currencyName, null);
+			
+			if (oldCurrencyRate != null) {				
+				Log.d("Debug1",oldCurrencyRate);
+				//compare old and new rates
+				Double newRate = Double.parseDouble(sellRate);
+				Double oldRate = Double.parseDouble(oldCurrencyRate);
+				if (oldRate.compareTo(newRate) >= 0){
+					ratesView.setImageViewResource(R.id.arrow, R.drawable.up);
+				} else {
+					ratesView.setImageViewResource(R.id.arrow, R.drawable.down);
+				}
+				if (!oldRate.equals(newRate)){
+					//update rates if changed
+					Log.d("Debug2",sellRate);
+					prefEditor.putString(currencyName, sellRate);	
+				}							
+			} else {
+				//first run, save rates
+				prefEditor.putString(currencyName, sellRate);						
+			}
+			prefEditor.commit();
+						
+			ratesView.setTextViewText(R.id.currencyName, currencyName);
+			ratesView.setTextViewText(R.id.buyRate, currencyRates.formatRate(buyRate) + ext);
+			ratesView.setTextViewText(R.id.sellRate, currencyRates.formatRate(sellRate) + ext); 	
+			currencyRatesView.addView(R.id.widget_layout, ratesView);
+		}		
 		
 		currencyRatesView.setOnClickPendingIntent(R.id.CurrencyRatesWidget, createPendingIntent(context));
 		
 		// Push update for this widget to the home screen
 		ComponentName thisAppWidget = new ComponentName(context.getPackageName(),getClass().getName());	
 		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-		appWidgetManager.updateAppWidget(thisAppWidget, currencyRatesView);		
+		appWidgetManager.updateAppWidget(thisAppWidget, currencyRatesView);			
 	}
 	
 	private PendingIntent createPendingIntent(Context context){
