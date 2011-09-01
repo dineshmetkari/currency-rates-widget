19aa7820b9d2bda902726c33a33a7a93a3104566
rpagyc@gmail.com <rpagyc@gmail.com@92d66d1c-334b-ccf3-5484-7130c893d296>
2011-09-01 08:39:38 +0000

    fixed arrow displaying, now takes previous state if rates not chaned

--- CurrencyRatesWidget/src/com/app/currencywidget/CurrencyRates.java
+++ CurrencyRatesWidget/src/com/app/currencywidget/CurrencyRates.java
@@ -17,67 +17,73 @@ import android.util.Log;
 import android.widget.LinearLayout;
 import com.app.R;
 
-public class CurrencyRates extends LinearLayout{
-	
+public class CurrencyRates extends LinearLayout {
+
 	private String currencyName;
 	private String sellRate;
 	private String buyRate;
 	private String ext;
-	
-	public CurrencyRates(Context context){
-		super(context);		
+
+	public CurrencyRates(Context context) {
+		super(context);
 	}
 
-	public ArrayList<CurrencyRates> getCurrencyRates(Context context){
+	public ArrayList<CurrencyRates> getCurrencyRates(Context context) {
 		ArrayList<CurrencyRates> currencyRatesList = new ArrayList<CurrencyRates>();
-		String[] currencyList = context.getResources().getStringArray(R.array.currencyList);
-		
-		try {
-        	URL url = new URL(context.getResources().getString(R.string.serviceUrl));
-        	DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        	DocumentBuilder db = dbf.newDocumentBuilder();
-        	Document doc = db.parse(new InputSource(url.openStream()));
-        	doc.getDocumentElement().normalize();
-        	
-        	NodeList nodeList = doc.getElementsByTagName("currency");
-        	
-        	for (int i = 0; i < nodeList.getLength(); i++){
-        		
-        		Element currencyElement = (Element)nodeList.item(i);       		
-        		
-        		for (int j = 0; j < currencyList.length; j++){
-        			
-        			if (currencyList[j].equals(currencyElement.getAttribute("name"))){        				
-        			        				
-        				String currencyName = currencyElement.getAttribute("name").replace("EURO",	"EUR"); // fix for EUR
-        				String buyRate = currencyElement.getAttribute("buy").replace(',','.');
-        				String sellRate = currencyElement.getAttribute("sell").replace(',','.');
-        				String ext = currencyElement.getAttribute("ext");
+		String[] currencyList = context.getResources().getStringArray(
+				R.array.currencyList);
 
-        				CurrencyRates currencyRates = new CurrencyRates(context);
-        				currencyRates.setCurrencyName(currencyName);
-        				currencyRates.setSellRate(sellRate);
-        				currencyRates.setBuyRate(buyRate);
-        				currencyRates.setExt(ext);
-        				currencyRatesList.add(currencyRates);
-        			}
-        		}      	
-        	}
-        	
-        } catch (Exception e){
-        	Log.d("DEBUG","XML parsing exception: " + e);
-        }
+		try {
+			URL url = new URL(context.getResources().getString(
+					R.string.serviceUrl));
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(new InputSource(url.openStream()));
+			doc.getDocumentElement().normalize();
+
+			NodeList nodeList = doc.getElementsByTagName("currency");
+
+			for (int i = 0; i < nodeList.getLength(); i++) {
+
+				Element currencyElement = (Element) nodeList.item(i);
+
+				for (int j = 0; j < currencyList.length; j++) {
+
+					if (currencyList[j].equals(currencyElement
+							.getAttribute("name"))) {
+
+						String currencyName = currencyElement.getAttribute(
+								"name").replace("EURO", "EUR"); // fix for EUR
+						String buyRate = currencyElement.getAttribute("buy")
+								.replace(',', '.');
+						String sellRate = currencyElement.getAttribute("sell")
+								.replace(',', '.');
+						String ext = currencyElement.getAttribute("ext");
+
+						CurrencyRates currencyRates = new CurrencyRates(context);
+						currencyRates.setCurrencyName(currencyName);
+						currencyRates.setSellRate(sellRate);
+						currencyRates.setBuyRate(buyRate);
+						currencyRates.setExt(ext);
+						currencyRatesList.add(currencyRates);
+					}
+				}
+			}
+
+		} catch (Exception e) {
+			Log.d("DEBUG", "XML parsing exception: " + e);
+		}
 		return currencyRatesList;
 	}
-	
-	public String formatRate(String rate){
+
+	public String formatRate(String rate) {
 		DecimalFormat df = new DecimalFormat("0.00");
-		
-		if (rate != null){			
+
+		if (rate != null) {
 			Double dRate = Double.parseDouble(rate);
 			rate = String.valueOf(df.format(dRate));
 		}
-		
+
 		return rate;
 	}
 
--- CurrencyRatesWidget/src/com/app/currencywidget/CurrencyRatesWidget.java
+++ CurrencyRatesWidget/src/com/app/currencywidget/CurrencyRatesWidget.java
@@ -18,99 +18,121 @@ import android.widget.RemoteViews;
 import com.app.R;
 
 public class CurrencyRatesWidget extends AppWidgetProvider {
-	
+
 	private static final String ACTION_WIDGET_RECEIVER = "com.app.currencywidget.ACTION_WIDGET_RECEIVER";
-	private RemoteViews currencyRatesView = new RemoteViews("com.app", R.layout.widget_layout);
+	private RemoteViews currencyRatesView = new RemoteViews("com.app",
+			R.layout.widget_layout);
 	private CurrencyRates currencyRates;
 	private ArrayList<CurrencyRates> currencyRatesList = new ArrayList<CurrencyRates>();
 	private SharedPreferences prefs;
-		
-	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
-		updateWidget(context);	
-	}		
-	
-	@Override
-	public void onEnabled(Context context){		
-		super.onEnabled(context);		
-		AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);		
-		alarmManager.setRepeating(AlarmManager.RTC, SystemClock.elapsedRealtime() + 360000, 360000, createPendingIntent(context));		
+
+	private PendingIntent createPendingIntent(Context context) {
+		Intent intent = new Intent(context, CurrencyRatesWidget.class);
+		intent.setAction(ACTION_WIDGET_RECEIVER);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0,
+				intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		return pendingIntent;
 	}
-	
-	@Override 
-	public void onDisabled(Context context){					
+
+	@Override
+	public void onDisabled(Context context) {
 		super.onDisabled(context);
-		AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+		AlarmManager alarmManager = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
 		alarmManager.cancel(createPendingIntent(context));
 	}
-	
+
 	@Override
-	public void onReceive(Context context, Intent intent){			
-		super.onReceive(context, intent);		
-		if(intent.getAction().equals(ACTION_WIDGET_RECEIVER)){			
-			updateWidget(context);	
+	public void onEnabled(Context context) {
+		super.onEnabled(context);
+		AlarmManager alarmManager = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
+		alarmManager.setRepeating(AlarmManager.RTC,
+				SystemClock.elapsedRealtime() + 360000, 360000,
+				createPendingIntent(context));
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		super.onReceive(context, intent);
+		if (intent.getAction().equals(ACTION_WIDGET_RECEIVER)) {
+			updateWidget(context);
 		}
 	}
-	
-	public void updateWidget(Context context){
-		
-		currencyRates = new CurrencyRates(context);		
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		updateWidget(context);
+	}
+
+	public void updateWidget(Context context) {
+
+		currencyRates = new CurrencyRates(context);
 		currencyRatesList = currencyRates.getCurrencyRates(context);
-				
+
 		prefs = PreferenceManager.getDefaultSharedPreferences(context);
 		SharedPreferences.Editor prefEditor = prefs.edit();
-		
+
 		currencyRatesView.removeAllViews(R.id.widget_layout);
-		
-		for (int i = 0; i < currencyRatesList.size(); i++){
-			
-			RemoteViews ratesView = new RemoteViews(context.getPackageName(),R.layout.rates_frame_layout);
-			
+
+		for (int i = 0; i < currencyRatesList.size(); i++) {
+
+			RemoteViews ratesView = new RemoteViews(context.getPackageName(),
+					R.layout.rates_frame_layout);
+
 			String currencyName = currencyRatesList.get(i).getCurrencyName();
 			String buyRate = currencyRatesList.get(i).getBuyRate();
 			String sellRate = currencyRatesList.get(i).getSellRate();
 			String ext = currencyRatesList.get(i).getExt();
 			String oldCurrencyRate = prefs.getString(currencyName, null);
-			
-			if (oldCurrencyRate != null) {				
-				Log.d("Debug1",oldCurrencyRate);
-				//compare old and new rates
+
+			if (oldCurrencyRate != null) {
+				Log.d("Debug1", oldCurrencyRate);
+				// compare old and new rates
 				Double newRate = Double.parseDouble(sellRate);
 				Double oldRate = Double.parseDouble(oldCurrencyRate);
-				if (oldRate.compareTo(newRate) >= 0){
+
+				if (oldRate.compareTo(newRate) > 0) {
 					ratesView.setImageViewResource(R.id.arrow, R.drawable.up);
-				} else {
+					prefEditor.putString(currencyName, sellRate);
+					prefEditor.putString("arrow", "up");
+				} else if (oldRate.compareTo(newRate) < 0) {
 					ratesView.setImageViewResource(R.id.arrow, R.drawable.down);
+					prefEditor.putString(currencyName, sellRate);
+					prefEditor.putString("arrow", "down");
+				} else {
+					if (prefs.getString("arrow", "up").equals("down")) {
+						ratesView.setImageViewResource(R.id.arrow,
+								R.drawable.down);
+					} else {
+						ratesView.setImageViewResource(R.id.arrow,
+								R.drawable.up);
+					}
 				}
-				if (!oldRate.equals(newRate)){
-					//update rates if changed
-					Log.d("Debug2",sellRate);
-					prefEditor.putString(currencyName, sellRate);	
-				}							
+
 			} else {
-				//first run, save rates
-				prefEditor.putString(currencyName, sellRate);						
+				// first run, save rates
+				prefEditor.putString(currencyName, sellRate);
 			}
 			prefEditor.commit();
-						
+
 			ratesView.setTextViewText(R.id.currencyName, currencyName);
-			ratesView.setTextViewText(R.id.buyRate, currencyRates.formatRate(buyRate) + ext);
-			ratesView.setTextViewText(R.id.sellRate, currencyRates.formatRate(sellRate) + ext); 	
+			ratesView.setTextViewText(R.id.buyRate,
+					currencyRates.formatRate(buyRate) + ext);
+			ratesView.setTextViewText(R.id.sellRate,
+					currencyRates.formatRate(sellRate) + ext);
 			currencyRatesView.addView(R.id.widget_layout, ratesView);
-		}		
-		
-		currencyRatesView.setOnClickPendingIntent(R.id.CurrencyRatesWidget, createPendingIntent(context));
-		
+		}
+
+		currencyRatesView.setOnClickPendingIntent(R.id.CurrencyRatesWidget,
+				createPendingIntent(context));
+
 		// Push update for this widget to the home screen
-		ComponentName thisAppWidget = new ComponentName(context.getPackageName(),getClass().getName());	
-		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-		appWidgetManager.updateAppWidget(thisAppWidget, currencyRatesView);			
-	}
-	
-	private PendingIntent createPendingIntent(Context context){
-		Intent intent = new Intent(context, CurrencyRatesWidget.class);
-		intent.setAction(ACTION_WIDGET_RECEIVER);
-		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-		return pendingIntent;
+		ComponentName thisAppWidget = new ComponentName(
+				context.getPackageName(), getClass().getName());
+		AppWidgetManager appWidgetManager = AppWidgetManager
+				.getInstance(context);
+		appWidgetManager.updateAppWidget(thisAppWidget, currencyRatesView);
 	}
 }
